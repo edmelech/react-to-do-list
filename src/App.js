21a8357841aa14fb36eef2d5907a272e3aad3e5b
Ed Melech <mr.melech@gmail.com>
2021-09-06 15:15:55 +0100

    set up onClick delete function

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <h1>Ed's Todo list</h1>
       </header>
       <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-      <TodoList todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
