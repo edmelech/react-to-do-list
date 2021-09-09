b160723ba1ce05c6c2010b19efacd4cadf4b7f1b
Ed Melech <mr.melech@gmail.com>
2021-09-09 18:24:53 +0100

    create filterHandler function

--- src/App.js
+++ src/App.js
@@ -9,9 +9,21 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
-  const [filteredTodos, setDilteredTodos] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
   // Functions
-  
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default: 
+        setFilteredTodos(todos);
+        break;
+    }
+  }
   return (
     <div className="App">
       <header>
