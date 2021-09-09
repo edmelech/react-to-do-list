bda0bd97f30ec6ae3b77559af0697f72334dc169
Ed Melech <mr.melech@gmail.com>
2021-09-09 18:41:33 +0100

    add useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 //Importing Components
 import Form from "./components/Form";
@@ -10,6 +10,11 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //USE EFFECT
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status])
   // Functions
   const filterHandler = () => {
     switch(status){
